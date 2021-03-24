9bd8ea9729edf37f1063c4a9b42dd28f9e77cce1
rigoriosh <rigoriosh@gmail.com>
2021-03-24 17:08:00 -0500

    end mySql node

--- 11-typs-restserver/controllers/usuariosController.ts
+++ 11-typs-restserver/controllers/usuariosController.ts
@@ -1,42 +1,82 @@
 import { Request, Response } from "express";
+import Usuario from '../models/usuarioModel';
 
-export const getUsuarios = (req: Request, res: Response) => {
-      res.json({
-         msg: 'getUsuarios'
-     })
+export const getUsuarios = async (req: Request, res: Response) => {
+    const usuarios = await Usuario.findAll();
+    res.json({ usuarios });
 }
 
-export const getUsuario = (req: Request, res: Response) => {
-    const {id} = req.params;
-    res.json({
-       msg: 'getUsuario',
-       id
-   })
+export const getUsuario = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const usuario = await Usuario.findByPk(id);
+    if (usuario) {
+        res.json({
+            msg: 'getUsuario',
+            id,
+            usuario
+        })
+    } else {
+        res.status(400).json({ msg: `Usuario con id: ${id}, no exisiste` })
+    }
 }
 
-export const postUsuario = (req: Request, res: Response) => {
-    const {body} = req;
-    console.log(body);
-    res.json({
-       msg: 'postUsuario',
-       body
-   })
+// type usuario = { nombre: string, email: string,  estado: boolean}
+
+export const postUsuario = async (req: Request, res: Response) => { // crear usuario
+    const { body } = req;
+    try {
+        const existeEmail = await Usuario.findOne({ where: { email: body.email } });
+        if (existeEmail) return res.status(400).json({ msg: 'El correo ya esxite' });
+
+        const usuario = new Usuario(body);
+        await usuario.save();
+        res.json({
+            msg: 'postUsuario',
+            usuario
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' })
+    }
 }
 
-export const putUsuario = (req: Request, res: Response) => { // put === update
-    const {id} = req.params;
-    const {body} = req;
-    res.json({
-       msg: 'updated Usuario',
-       body,
-       id
-   })
+export const putUsuario = async (req: Request, res: Response) => { // put === update
+    const { id } = req.params;
+    const { body } = req;
+    try {
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) return res.status(400).json({ msg: 'El usuario no existe' });
+
+        await usuario.update(body);
+        res.json({
+            msg: 'updated Usuario',
+            body,
+            id
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' })
+    }
 }
 
-export const deleteUsuario = (req: Request, res: Response) => {
-    const {id} = req.params;
-    res.json({
-       msg: 'deleteUsuario',
-       id
-   })
+export const deleteUsuario = async(req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) return res.status(400).json({ msg: 'El usuario no existe' });
+
+        /* forma de eliminar totalmente de la db */
+        //usuario.destroy();
+        /* forma de elimiar registro manteniendo la integridad referencia de la DB */
+        await usuario.update({estado: false});
+
+        res.json({
+            msg: 'deleteUsuario',
+            usuario
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' })
+    }
+    
 }
--- 11-typs-restserver/dist/controllers/usuariosController.js
+++ 11-typs-restserver/dist/controllers/usuariosController.js
@@ -1,45 +1,98 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
-const getUsuarios = (req, res) => {
-    res.json({
-        msg: 'getUsuarios'
-    });
-};
+const usuarioModel_1 = __importDefault(require("../models/usuarioModel"));
+const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarios = yield usuarioModel_1.default.findAll();
+    res.json({ usuarios });
+});
 exports.getUsuarios = getUsuarios;
-const getUsuario = (req, res) => {
+const getUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'getUsuario',
-        id
-    });
-};
+    const usuario = yield usuarioModel_1.default.findByPk(id);
+    if (usuario) {
+        res.json({
+            msg: 'getUsuario',
+            id,
+            usuario
+        });
+    }
+    else {
+        res.status(400).json({ msg: `Usuario con id: ${id}, no exisiste` });
+    }
+});
 exports.getUsuario = getUsuario;
-const postUsuario = (req, res) => {
+// type usuario = { nombre: string, email: string,  estado: boolean}
+const postUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    console.log(body);
-    res.json({
-        msg: 'postUsuario',
-        body
-    });
-};
+    try {
+        const existeEmail = yield usuarioModel_1.default.findOne({ where: { email: body.email } });
+        if (existeEmail)
+            return res.status(400).json({ msg: 'El correo ya esxite' });
+        const usuario = new usuarioModel_1.default(body);
+        yield usuario.save();
+        res.json({
+            msg: 'postUsuario',
+            usuario
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' });
+    }
+});
 exports.postUsuario = postUsuario;
-const putUsuario = (req, res) => {
+const putUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: 'updated Usuario',
-        body,
-        id
-    });
-};
+    try {
+        const usuario = yield usuarioModel_1.default.findByPk(id);
+        if (!usuario)
+            return res.status(400).json({ msg: 'El usuario no existe' });
+        yield usuario.update(body);
+        res.json({
+            msg: 'updated Usuario',
+            body,
+            id
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' });
+    }
+});
 exports.putUsuario = putUsuario;
-const deleteUsuario = (req, res) => {
+const deleteUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'deleteUsuario',
-        id
-    });
-};
+    try {
+        const usuario = yield usuarioModel_1.default.findByPk(id);
+        if (!usuario)
+            return res.status(400).json({ msg: 'El usuario no existe' });
+        /* forma de eliminar totalmente de la db */
+        //usuario.destroy();
+        /* forma de elimiar registro manteniendo la integridad referencia de la DB */
+        yield usuario.update({ estado: false });
+        res.json({
+            msg: 'deleteUsuario',
+            usuario
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'talk to manager' });
+    }
+});
 exports.deleteUsuario = deleteUsuario;
 //# sourceMappingURL=usuariosController.js.map
